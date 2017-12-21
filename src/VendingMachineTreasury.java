10f05cd355b71c17ebd301905f1b68967d9168a7
Shad Downey <shad@1stdibs.com>
2017-12-21 11:17:25 -0500

    initial commit

---
+++ src/VendingMachineTreasury.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+enum Coin {
+	DOLLAR(1.0),
+	QUARTER(0.25),
+	DIME(0.10),
+	NICKLE(0.05),
+	PENNY(0.01),
+	;
+
+	private Double value;
+
+	Coin(Double value) {
+		this.value = value;
+	}
+
+	public Double getValue() {
+		return this.value;
+	}
+}
+
+public class VendingMachineTreasury {
+	//TODO - Maintain State - How many Quarters/Dimes/Nickles/Pennies do I have?
+
+	public List<Coin> makeChange(Double price, Double payment) {
+		//TODO - Implement me!
+		return null;
+	}
+
+}
+
+
+//TODO Write a couple tests
+
