956f8ba57722f073866ce7e811af3ad6b58e3900
Shad Downey <shad@1stdibs.com>
2025-05-05 09:45:36 -0400

    Tweaks

--- src/VendingMachineTreasury.java
+++ src/VendingMachineTreasury.java
@@ -22,13 +22,12 @@ enum Coin {
 public class VendingMachineTreasury {
 	// TODO - Maintain State - How many Quarters/Dimes/Nickles/Pennies do I have?
 
+	// From the 'bank', make change
+	// e.g. if price was 75 and payment was List.of(DOLLAR), we might expect List.of(QUARTER) returned
 	public List<Coin> makeChange(Integer price, List<Coin> payment) {
 		// TODO - Implement me!
 		return null;
 	}
 
 }
-
-
-// TODO Write a couple tests
 
